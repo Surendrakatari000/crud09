b7df5cb04fb418c72af52dc356d5b24027677435
Surendrakatari000 <surendra@SurendraKatari>
2025-07-09 10:09:47 +0530

    pushing

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get '/products', to: 'products#index'
+  get '/products/new', to: 'products#new'
+  get 'products/:id/edit', to: 'products#edit'
+  delete '/products/:id', to: 'products#destroy'
+  get '/products/:id', to: 'products#show'
+  post '/products', to: 'products#create'
+  patch '/products/:id' ,to: 'products#update'
+end
